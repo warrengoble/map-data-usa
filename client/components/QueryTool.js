4c29c557b01fcfdf42e94dd8646ba53859c8e2c0
Warren Goble <warrengoble@users.noreply.github.com>
2019-12-16 16:02:21 -0700

    Add query UI boilerplate

---
+++ client/components/QueryTool.js
@@ -0,0 +1,18 @@
+export default () => {
+  return (
+    <div className="container">
+      <style jsx>
+        {`
+          .container {
+            width: 350px;
+            background: #444;
+          }
+        `}
+      </style>
+      <div>Query Tools</div>
+      <div>
+          Year
+      </div>
+    </div>
+  );
+};
