6c138fc9003a1f505b68f72fcd3da394ccca4e40
Warren Goble <warrengoble@users.noreply.github.com>
2019-12-16 22:46:19 -0700

    Added webpack loaders

--- next.config.js
+++ next.config.js
@@ -1,3 +1,37 @@
 module.exports = {
+  webpack: (config, { defaultLoaders, isServer }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: require("styled-jsx/webpack").loader,
+          options: {
+            type: "global"
+          }
+        }
+      ]
+    });
+
+    const inlineImageLimit = 8192;
+
+    config.module.rules.push({
+      test: /\.(gif|png|jpe?g)$/i,
+      use: [
+        {
+          loader: require.resolve("url-loader"),
+          options: {
+            limit: inlineImageLimit,
+            fallback: require.resolve("file-loader"),
+            publicPath: `/_next/static/images/`,
+            outputPath: `${isServer ? "../" : ""}static/images/`,
+            name: "[name]-[hash].[ext]"
+          }
+        }
+      ]
+    });
+
+    return config;
+  },
   target: "serverless"
 };
