f8a4fa49620fb3404f0ce466ade2ce6e152a47c5
Warren Goble <warrengoble@users.noreply.github.com>
2019-12-19 03:14:24 -0700

    Fixed scrollbar bug

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ export default class extends App {
             body {
               margin: 0;
               padding: 0;
+              overflow: hidden;
             }
           `}
         </style>
