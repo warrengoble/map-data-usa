f815ed6c36f91ab944de9310616245957bf91a0d
Warren Goble <warrengoble@users.noreply.github.com>
2019-12-19 00:28:10 -0700

    Store cleanups

--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 import { reaction } from "mobx";
 import { useLocalStore, useStaticRendering } from "mobx-react-lite";
 import { createContext } from "react";
+import { values } from "lodash/fp";
 
 import createStore from "./createStore";
 import feathers from "./feathers";
@@ -15,19 +16,18 @@ export const StoreProvider = ({ children }) => {
   const service = feathers();
   const store = useLocalStore(createStore(service));
 
+  // Get filter values from database
   if (!isServer) {
     service.find({ query: { filters: true } }).then(values => {
-      store.filters = values.reduce((a, v, i) => ({ ...a, [v]: false }), {});
+      store.filters = values.reduce((a, v, i) => ({ ...a, [v]: true }), {});
     });
   }
 
   // Trigger when state is updated
   reaction(
-    () => [store.year],
-    async titles => {
-      const res = await service.find({ query: { year: store.year } });
-
-      console.log("reaction", res);
+    () => [store.year, ...values(store.filters).map(v => v)],
+    async () => {
+      store.results = await service.find({ query: {} });
     }
   );
 
