4a7727ba06c98f720419e08f45a86ca723c2a250
Warren Goble <warrengoble@users.noreply.github.com>
2020-04-17 17:31:32 -0700

    Added new filter grouped components

---
+++ components/FilterCheckBox.js
@@ -0,0 +1,49 @@
+import { Button } from "antd";
+import { pipe, map, entries } from "lodash/fp";
+
+import ToggleControl from "./ToggleControl";
+
+export default ({ name, filterValues = {}, onChange = () => {} }) => {
+  //
+  return (
+    <div>
+      <style jsx>
+        {`
+          .optionsButtons {
+            display: flex;
+            justify-content: center;
+          }
+
+          .optionsCheckBoxContainer {
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+            overflow: auto;
+            background: #333;
+          }
+        `}
+      </style>
+      <div className="optionsButtons">
+        <Button style={{ flex: 0.5 }} onClick={() => filter.clearFilters()}>
+          Clear
+        </Button>
+        <Button style={{ flex: 0.5 }} onClick={() => store.selectAllFilters()}>
+          Select All
+        </Button>
+      </div>
+      <div className="optionsCheckBoxContainer">
+        {pipe(
+          entries,
+          map(([key, checked]) => (
+            <ToggleControl
+              key={key}
+              title={key}
+              onChange={(value) => onChange(key, value)}
+              checked={checked}
+            />
+          ))
+        )(filterValues)}
+      </div>
+    </div>
+  );
+};
---
+++ components/FilterSlider.js
@@ -0,0 +1,44 @@
+import { reduce, pipe, keys } from "lodash/fp";
+
+import SliderControl from "./SliderControl";
+
+export default ({ name, filterValues = {}, onChange = () => {} }) => {
+  const filterValuesOnly = keys(filterValues);
+
+  const min = reduce((a, v) => (v < a ? v : a), Infinity)(filterValuesOnly);
+  const max = reduce((a, v) => (v > a ? v : a), -Infinity)(filterValuesOnly);
+
+  return (
+    <div className="optionsSliderContainer">
+      <style jsx>
+        {`
+          .optionsSliderContainer {
+            background: rgba(0, 0, 0, 0.6);
+          }
+        `}
+      </style>
+      <SliderControl
+        min={min}
+        max={max}
+        step={null}
+        onChange={(v) => {
+          onChange(v);
+        }}
+        marks={reduce(
+          (a, v, i) => ({
+            ...a,
+            [v]: {
+              style: {
+                color: "#bbb",
+                fontSize: "0.75em",
+              },
+              label: v,
+            },
+          }),
+          {}
+        )(filterValuesOnly)}
+        dots
+      />
+    </div>
+  );
+};
